fc8eee1b00a11afb2d510b8e64e0afde47bcabbe
CodAres <u202122633@upc.edu.pe>
2025-04-03 01:44:14 -0500

    feat(router): wrap AppRouter with BrowserRouter and Redux provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'; // Importa el Provider de react-redux
+import  store from './auth/redux/store'; // Importa el store de Redux
+import { AppRouter } from './router/app.router'; // Importa AppRouter
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
