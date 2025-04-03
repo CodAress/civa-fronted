c66ad0e57c66469b5e202a92b798735cdc9fcb3f
CodAres <u202122633@upc.edu.pe>
2025-04-03 10:47:24 -0500

    feat(fleet): add features field to Bus entity

--- src/fleet/model/bus.entite.ts
+++ src/fleet/model/bus.entite.ts
@@ -4,12 +4,14 @@ export class Bus {
     licensePlate: string;
     brand: string;
     status: string;
+    features: string;
 
-    constructor(bus: { id?: number; number?: string; licensePlate?: string; brand?: string; status?: string }) {
+    constructor(bus: { id?: number; number?: string; licensePlate?: string; brand?: string; status?: string; features?: string }) {
         this.id = bus.id || 0;
         this.number = bus.number || '';
         this.licensePlate = bus.licensePlate || '';
         this.brand = bus.brand || '';
         this.status = bus.status || '';
+        this.features = bus.features || '';
     }
 }
